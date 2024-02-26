5ac48a1b8d5271026a89fcb05d37a011423d0ce1
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-02-26 14:58:33 -0600

    changes on script

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,6 @@ const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 
 
-
-
-
-
-
-
 function listTopics() {
  for (let x = 0; x < topics.length; x++) {
    console.log(topics[x]);
